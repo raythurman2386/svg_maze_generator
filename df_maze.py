c83639de0356c9437ebd11b53f890faf913bd937
rthurman2386 <raymondthurman5@gmail.com>
2021-08-06 09:56:41 -0400

    initial maze file for depth first

---
+++ df_maze.py
@@ -0,0 +1,47 @@
+import random
+
+
+# Create a maze using the depth-first algorithm described at
+# https://scipython.com/blog/making-a-maze/
+# Christian Hill, April 2017.
+
+class Cell:
+    """A cell in the maze.
+
+    A maze "Cell" is a point in the grid which may be surrounded by walls to
+    the north, east, south or west.
+
+    """
+
+    # A wall separates a pair of cells in the N-S or W-E directions.
+    wall_pairs = {'N': 'S', 'S': 'N', 'E': 'W', 'W': 'E'}
+
+    def __init__(self, x, y):
+        """Initialize the cell at (x,y). At first it is surrounded by walls."""
+
+        self.x, self.y = x, y
+        self.walls = {'N': True, 'S': True, 'E': True, 'W': True}
+
+
+class Maze:
+    """A Maze, represented as a grid of cells."""
+
+    def __init__(self, nx, ny, ix=0, iy=0):
+        """Initialize the maze grid.
+        The maze consists of nx x ny cells and will be constructed starting
+        at the cell indexed at (ix, iy).
+
+        """
+
+        self.nx, self.ny = nx, ny
+        self.ix, self.iy = ix, iy
+        self.maze_map = [[Cell(x, y) for y in range(ny)] for x in range(nx)]
+
+    def cell_at(self, x, y):
+        """Return the Cell object at (x,y)."""
+
+        return self.maze_map[x][y]
+
+    def __str__(self):
+        """Return a (crude) string representation of the maze."""
+        pass
